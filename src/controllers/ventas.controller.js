f6c38d4a599dea0107e3e0f00d4842c666275a51
Vivero Abidan <viveroabidan@gmail.com>
2026-05-09 18:00:25 -0600

    Quitar IVA automatico en pagos con tarjeta

--- src/controllers/ventas.controller.js
+++ src/controllers/ventas.controller.js
@@ -424,12 +424,9 @@ export const crearVenta = async (req, res) => {
 
       const totalFinalItem = Math.max(subtotal - descuentoItem, 0);
 
-      const ivaItem =
-        esTarjeta && Number(p.iva_tarjeta) === 1
-          ? Number((totalFinalItem * 0.16).toFixed(2))
-          : 0;
+      const ivaItem = 0;
 
-      totalIVA += ivaItem;
+totalIVA += ivaItem;
 
       await conn.query(
         `INSERT INTO ventas_items
